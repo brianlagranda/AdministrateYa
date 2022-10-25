f0a9e2d4b71efe3605811fc585fe5e7e11994e18
Brian <brianlagranda@gmail.com>
2022-10-25 08:30:39 -0300

    bienvenida done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-alert("Testeando JS");
+function bienvenida(){
+    let nombreUsuario = prompt("Ingrese su nombre: ");
+    let saludo = alert(`Bienvenido, ${nombreUsuario}`);
+    return saludo;
+}
+
+bienvenida();
