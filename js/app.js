471900d48d66af224f1c015dda9ef21ad32aacaa
Brian <brianlagranda@gmail.com>
2022-10-25 11:38:38 -0300

    Adding new features app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
-function bienvenida(){
-    let nombreUsuario = prompt("Ingrese su nombre: ");
-    let saludo = alert(`Bienvenido, ${nombreUsuario}`);
-    return saludo;
+class Usuario{
+    constructor(id, nombreUsuario, balance) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.balance = balance;
+    }
+
+    saludo(){
+        console.log(`Bienvenido, ${this.nombreUsuario}`);
+    }
+
+    mostrarBalance(balance){
+        console.log(`Balance: $ ${balance}`)
+    }
+}
+
+function solicitarNombreUsuario(){
+    let nombreUsuario = prompt("Ingrese nombre de usuario: "); 
+    return nombreUsuario;
 }
 
 bienvenida();
+
+function mostrarBalance(nombreUsuario){
+    // TO DO - Mostrar el balance dependiendo el id del usuario.
+}
+
+const usuarioPrueba = new Usuario(1, "Prueba", 10000);
+
+usuarioPrueba.mostrarBalance();
