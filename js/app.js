01d9713189f9468b9cd32c5d13bacd40cb62aad9
Brian <brianlagranda@gmail.com>
2022-10-25 13:44:11 -0300

    Added function solicitarOperacion

--- js/app.js
+++ js/app.js
@@ -5,13 +5,20 @@ class Usuario{
         this.balance = balance;
     }
 
-    saludo(){
-        console.log(`Bienvenido, ${this.nombreUsuario}`);
+    bienvenida(){
+        console.log(`Bienvenid@, ${this.nombreUsuario}`);
     }
 
-    mostrarBalance(balance){
-        console.log(`Balance: $ ${balance}`)
+    mostrarBalance(){
+        console.log(`Su Balance es: $ ${this.balance}`)
     }
+}
+
+function solicitarOperacion(){
+    let operacion = Number(prompt("Ingrese el número de operación:\n1-Mostrar Balance\n2-Cargar un ingreso\n3-Cargar un egreso"));
+    while (operacion!=1 && operacion!=2 && operacion!=3)
+        operacion = Number(prompt("El número ingresado es incorrecto, por favor seleccione una de las siguientes opciones (Entre 1-3):\n1-Mostrar Balance\n2-Cargar un ingreso\n3-Cargar un egreso"));
+    return operacion;
 }
 
 function solicitarNombreUsuario(){
@@ -19,12 +26,13 @@ function solicitarNombreUsuario(){
     return nombreUsuario;
 }
 
-bienvenida();
-
 function mostrarBalance(nombreUsuario){
     // TO DO - Mostrar el balance dependiendo el id del usuario.
 }
 
 const usuarioPrueba = new Usuario(1, "Prueba", 10000);
 
+usuarioPrueba.bienvenida();
 usuarioPrueba.mostrarBalance();
+
+console.log(solicitarOperacion());
